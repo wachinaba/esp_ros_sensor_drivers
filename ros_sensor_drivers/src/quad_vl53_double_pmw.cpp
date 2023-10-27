8763a350a530c862bfd5a06df3a5ae259a6006f3
wachinaba <wachinaba@gmail.com>
2023-10-27 20:05:01 +0900

    add pmw3901の読み取り

--- ros_sensor_drivers/src/quad_vl53_double_pmw.cpp
+++ ros_sensor_drivers/src/quad_vl53_double_pmw.cpp
@@ -1,4 +1,5 @@
 #include "VL53Manager.h"
+#include <PMW3901.h>
 #include <Wire.h>
 #include <ros.h>
 #include <multizone_lidar_msgs/MultizoneRange.h>
@@ -6,13 +7,20 @@
 #define BAUDRATE 230400
 #define I2C_FREQ 1000000
 
+SPIClass vspi(VSPI);
+
 std::vector<uint8_t> sensor_addresses_wire0{0x40, 0x41};
 std::vector<uint8_t> sensor_addresses_wire1{0x42, 0x43};
 std::vector<uint8_t> sensor_reset_pins{32, 33};
 
+std::vector<uint8_t> flow_sensor_cs_pins{2, 4};
+
 VL53Manager vl53_manager_wire0(&Wire, sensor_addresses_wire0, sensor_reset_pins);
 VL53Manager vl53_manager_wire1(&Wire1, sensor_addresses_wire1, sensor_reset_pins);
 VL53L5CX_ResultsData measurement_data = VL53L5CX_ResultsData();
+
+std::vector<PMW3901> flow_sensors{PMW3901(2, &vspi), PMW3901(4, &vspi)};
+std::vector<bool> flow_sensor_ready{false, false};
 
 void setup()
 {
@@ -28,6 +36,34 @@ void setup()
 
   vl53_manager_wire0.beginSensors();
   vl53_manager_wire1.beginSensors();
+
+  for (uint8_t i = 0; i < flow_sensors.size(); i++)
+  {
+    for (uint8_t j = 0; j < flow_sensors.size(); j++)
+    {
+      digitalWrite(flow_sensor_cs_pins[j], HIGH);
+    }
+
+    delay(100);
+    digitalWrite(flow_sensor_cs_pins[i], LOW);
+    delay(1);
+
+    flow_sensor_ready[i] = flow_sensors[i].begin();
+    if (flow_sensor_ready[i])
+    {
+      Serial.println("Flow sensor " + String(i) + " initialized");
+      flow_sensors[i].setLed(true);
+    }
+    else
+    {
+      Serial.println("Flow sensor " + String(i) + " initialization failed");
+    }
+  }
+
+  for (uint8_t i = 0; i < flow_sensors.size(); i++)
+  {
+    digitalWrite(flow_sensor_cs_pins[i], HIGH);
+  }
 
   Serial.println("VL53L5CX initialized");
 }
@@ -68,5 +104,32 @@ void loop()
       Serial.println();
     }
   }
+
+  for (uint8_t i = 0; i < flow_sensors.size(); i++)
+  {
+    if (!flow_sensor_ready[i])
+    {
+      continue;
+    }
+
+    int16_t deltaX, deltaY;
+    uint8_t squal;
+
+    digitalWrite(flow_sensor_cs_pins[i], LOW);
+    delay(1);
+
+    flow_sensors[i].readMotionCount(&deltaX, &deltaY, &squal);
+
+    digitalWrite(flow_sensor_cs_pins[i], HIGH);
+    delay(1);
+
+    Serial.print("Flow sensor " + String(i) + " delta x: ");
+    Serial.print(deltaX);
+    Serial.print(" delta y: ");
+    Serial.print(deltaY);
+    Serial.print(" squal: ");
+    Serial.println(squal);
+  }
+
   delay(1);
 }
